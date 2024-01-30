a0ccf4a012b7241d8a073add1a53b4e81bf1bc82
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2024-01-30 15:08:16 +0200

    build issu

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import dynamic from "next/dynamic";
 import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
